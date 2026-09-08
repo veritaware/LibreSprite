7ba42d7a510ea9c266800ef71fe43e39a6496727
Daniel Praźmo <d.prazmo@icloud.com>
2026-09-08 17:11:31 +0200

    Update spacing
    NO_SW_CHANGE

--- src/ui/widget.h
+++ src/ui/widget.h
@@ -1,6 +1,6 @@
-// Aseprite    | Copyright (C) 2001-2016  David Capello
-// LibreSprite | Copyright (C)      2021  LibreSprite contributors
-// Besprited   | Copyright (C)      2026  Veritaware
+// Aseprite    | Copyright (C) 2001-2016 David Capello
+// LibreSprite | Copyright (C) 2021      LibreSprite contributors
+// Besprited   | Copyright (C) 2026      Veritaware
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
