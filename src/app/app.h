66aa7c98502fbbaa98d1bd7820945e0bb84ad565
Daniel Praźmo <d.prazmo@icloud.com>
2026-09-08 18:03:36 +0200

    Fix spacing
    NO_SW_CHANGE

--- src/app/app.h
+++ src/app/app.h
@@ -1,6 +1,6 @@
-// Aseprite    | Copyright (C) 2001-2016  David Capello
-// LibreSprite | Copyright (C) 2021       LibreSprite contributors
-// Besprited   | Copyright (C) 2026       Veritaware
+// Aseprite    | Copyright (C) 2001-2016 David Capello
+// LibreSprite | Copyright (C) 2021      LibreSprite contributors
+// Besprited   | Copyright (C) 2026      Veritaware
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License version 2 as
