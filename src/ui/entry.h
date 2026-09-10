a0125e9543c42a8bd143edb923c338a22c0a6a8d
Daniel Praźmo <d.prazmo@icloud.com>
2026-09-10 13:14:15 +0200

    Fix heading
    NO_SW_CHANGE

--- src/ui/entry.h
+++ src/ui/entry.h
@@ -1,6 +1,6 @@
-// Aseprite UI Library
-// Copyright (C) 2001-2013, 2015  David Capello
-// Besprited | Copyright (C) 2026 Veritaware
+// UI Library
+// Aseprite  | Copyright (C) 2001-2015 David Capello
+// Besprited | Copyright (C) 2026      Veritaware
 //
 // This file is released under the terms of the MIT license.
 // Read LICENSE.txt for more information.
